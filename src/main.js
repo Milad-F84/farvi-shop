ca3aeee23ceea76a9862f467675626045f5000f9
miladfarajollahi <mfarajollahifrontend@gmail.com>
2024-11-13 20:14:04 +0330

    skeleton complete

--- src/main.js
+++ src/main.js
@@ -8,18 +8,20 @@ async function getLimitedProducts(limitCount = 4) {
 
 async function renderMainPageProducts() {
   const products = await getLimitedProducts(4);
-  const template = products.map((product) => {
-    const {title,image,price} = product
-    return `
-    <div class="flex flex-col items-center rounded-lg shadow-xl pb-4 overflow-hidden">
+  const template = products
+    .map((product) => {
+      const { title, image, price } = product;
+      return `
+    <div class="w-full flex flex-col items-center rounded-lg shadow-xl pb-4 overflow-hidden">
     <img class="w-full aspect-square object-cover" src="${image}" alt="" width="400px">
-    <h2 class="font-bold text-xl mt-4">${title}</h2>
+    <h2 class="font-bold text-xl mt-4 text-center line-clamp-1">${title}</h2>
     <div class="flex gap-1 mt-4">
     <span>${price}</span>
     <span>تومان</span>
     </div>
-    </div>`
-  }).join("");
+    </div>`;
+    })
+    .join("");
   mainPageProductsContainer.innerHTML = template;
 }
 renderMainPageProducts();
